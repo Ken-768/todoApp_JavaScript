739e62f7152d6b9b082459545dd2e7190af041ab
nakanokentarou <kcp.is.revolutionary@gmail.com>
2020-05-10 17:28:22 +0900

    アニメーションをつけた

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 //event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 //functions
 function addTodo(event) {
@@ -46,7 +48,10 @@ function deleteCheck(e) {
     //delete todo
     if(item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
-        todo.remove();
+        todo.classList.add("fall");
+        todo.addEventListener('transitioned', function() {
+            todo.remove();
+        });
     }
 
     //check mark
@@ -54,4 +59,31 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach((todo) => {
+      if (todo.classList !== undefined) {
+        switch (e.target.value) {
+          case "all":
+            todo.style.display = "flex";
+            break;
+          case "completed":
+            if (todo.classList.contains("completed")) {
+              todo.style.display = "flex";
+            } else {
+              todo.style.display = "none";
+            }
+            break;
+          case "uncompleted":
+            if (!todo.classList.contains("completed")) {
+                todo.style.display = "flex";
+            } else {
+                todo.style.display = "none";
+            }
+        }
+      }
+      return;
+    });
 }
